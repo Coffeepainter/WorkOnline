d84ef437465d1cb2adde7ca521b91684e196ec1c
diyar <68123026+Coffeepainter@users.noreply.github.com>
2021-05-23 01:25:55 +0430

    The Work Online is a First Beautiful website
    Nothing...

---
+++ offline.js
@@ -0,0 +1,21 @@
+﻿{
+	"version": 1621716516,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-3.4.1.min.js",
+		"offlineClient.js",
+		"bg_home-sheet0.png",
+		"button-sheet0.png",
+		"telegram-sheet0.png",
+		"instagram-sheet0.png",
+		"whatsapp-sheet0.png",
+		"backe-sheet0.png",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png"
+	]
+}
